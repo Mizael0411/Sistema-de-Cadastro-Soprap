5907be9ac2ee31f5cbb7f9d92ac935f507e4f592
MariaLyoshiura <yuyu.yoshiura@gmail.com>
2026-09-14 11:09:52 -0300

    Alterações do banco de dados

---
+++ src/main/java/com/abrigo/database/JPAUtil.java
@@ -0,0 +1,35 @@
+package com.abrigo.database;
+
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityManagerFactory;
+import jakarta.persistence.Persistence;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Properties;
+
+public class JPAUtil {
+
+    private static final EntityManagerFactory FACTORY;
+
+    static {
+        Map<String, String> properties = new HashMap<>();
+        Properties prop = new Properties();
+
+        try (FileInputStream fis = new FileInputStream("config.properties")) {
+            prop.load(fis);
+            properties.put("jakarta.persistence.jdbc.url", prop.getProperty("db.url"));
+            properties.put("jakarta.persistence.jdbc.user", prop.getProperty("db.user"));
+            properties.put("jakarta.persistence.jdbc.password", prop.getProperty("db.password"));
+        } catch (IOException e) {
+            System.err.println("Arquivo config.properties não encontrado na raiz do projeto!");
+        }
+
+        FACTORY = Persistence.createEntityManagerFactory("soprap-pu", properties);
+    }
+
+    public static EntityManager getEntityManager() {
+        return FACTORY.createEntityManager();
+    }
+}
---
+++ src/main/java/com/abrigo/model/TesteAnimal.java
@@ -0,0 +1,35 @@
+package com.abrigo.model;
+
+import com.abrigo.database.JPAUtil;
+import jakarta.persistence.EntityManager;
+import java.time.LocalDate;
+
+public class TesteAnimal {
+    public static void main(String[] args) {
+        Animal animal = new Animal();
+        animal.setNome("puppy");
+        animal.setIdade(3);
+        animal.setDataNascimento(LocalDate.of(2023, 1, 15));
+        animal.setSexo("Macho");
+        animal.setStatusVacinacao("Vacinado");
+        animal.setStatusGravidez("Não se aplica");
+        animal.setDataUltimaVacinacao(LocalDate.now());
+
+        EntityManager em = JPAUtil.getEntityManager();
+
+        try {
+            em.getTransaction().begin();
+            em.persist(animal);
+            em.getTransaction().commit();
+            System.out.println("Sucesso: Tabela criada e animal salvo no Supabase!");
+        } catch (Exception e) {
+            if (em.getTransaction().isActive()) {
+                em.getTransaction().rollback();
+            }
+            System.err.println("Ocorreu um erro na persistência:");
+            e.printStackTrace();
+        } finally {
+            em.close();
+        }
+    }
+}
